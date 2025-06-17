5f3cc4f9807abe0f016a0dadf8c5d17707e932a0
cbeaufil <cbeaufil@42lehavre.fr>
2025-06-17 23:13:05 +0200

    WIP: forge header, TODO: fix segfault

--- tests/main.c
+++ tests/main.c
@@ -1,6 +1,15 @@
 #include "../includes/armalloc.h"
 
 int main() {
-    printf("%p\n", ft_malloc(0x3339));
+    char *test1 = ft_malloc(0x50);
+    char *test2 = ft_malloc(0x50);
+
+    printf("1: %p\n", test1);
+    printf("2: %p\n", test2);
+
+    sprintf("AAAAA", test1);
+    sprintf("BBBBB", test2);
+
+
     return 0;
 }
