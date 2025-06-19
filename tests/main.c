f222835135838183082d7269778be0ad6627e0ef
cbeaufil <cbeaufil@localhost.localdomain>
2025-06-19 17:39:31 +0200

    DONE: pop freed chunk from binlist

--- tests/main.c
+++ tests/main.c
@@ -4,29 +4,30 @@
 int main() {
 
     // for (size_t i = 0 ; i < 0x10; i ++) {
-    //     char *p = ft_malloc(0x60);
+    //     char *p = ft_malloc(0x50);
     //     if (p) {
     //         strcpy(p,  "AAAAA");
     //     }
     //     printf(">> %p\n", p );
     // }
 
-    char *p1 = ft_malloc(0x60);
+    char *p1 = ft_malloc(0x50);
             // strcpy(p,  "AAAAA");
 
-    char *p2 = ft_malloc(0x60);
-    char *p3 = ft_malloc(0x60);
-    char *p4 = ft_malloc(0x60);
+    char *p2 = ft_malloc(0x50);
+    char *p3 = ft_malloc(0x50);
+    char *p4 = ft_malloc(0x50);
 
     ft_free(p1);
     ft_free(p2);
     ft_free(p3);
     ft_free(p4);
-    // printf("======CHUNK HEADER======\n");
-    // char *p = ft_malloc(0x50);
 
 
-    // printf("SIZE: %lx    | FLAG: %lx\n", (unsigned long)*(p-0x10), (unsigned long)*(p-0x8));
+    p1 = ft_malloc(0x50);
+    p2 = ft_malloc(0x50);
+    p3 = ft_malloc(0x50);
+    p4 = ft_malloc(0x50);
 
     return 0;
 }
