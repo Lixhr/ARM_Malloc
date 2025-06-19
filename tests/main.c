51a1ab32b6e44a29a66a17bc50e67b0fd2711c19
cbeaufil <cbeaufil@localhost.localdomain>
2025-06-19 18:00:36 +0200

    DONE: Pop, TODO: Handle multiple sizes

--- tests/main.c
+++ tests/main.c
@@ -11,23 +11,33 @@ int main() {
     //     printf(">> %p\n", p );
     // }
 
-    char *p1 = ft_malloc(0x50);
-            // strcpy(p,  "AAAAA");
+    // char *p1 = ft_malloc(0x50);
+    //         // strcpy(p,  "AAAAA");
 
-    char *p2 = ft_malloc(0x50);
-    char *p3 = ft_malloc(0x50);
-    char *p4 = ft_malloc(0x50);
+    // char *p2 = ft_malloc(0x50);
+    // char *p3 = ft_malloc(0x50);
+    // char *p4 = ft_malloc(0x50);
+
+    // ft_free(p1);
+    // ft_free(p2);
+    // ft_free(p3);
+    // ft_free(p4);
+
+
+    // p1 = ft_malloc(0x50);
+    // p2 = ft_malloc(0x50);
+    // p3 = ft_malloc(0x50);
+    // p4 = ft_malloc(0x50);
+
+
+    char *p1 = ft_malloc(0x150);
+    char *p2 = ft_malloc(0x150);
+    char *p3 = ft_malloc(0x150);
+
 
     ft_free(p1);
     ft_free(p2);
     ft_free(p3);
-    ft_free(p4);
-
-
-    p1 = ft_malloc(0x50);
-    p2 = ft_malloc(0x50);
-    p3 = ft_malloc(0x50);
-    p4 = ft_malloc(0x50);
 
     return 0;
 }
