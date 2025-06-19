21c877611b583a8c31c50bc3fd8d2809bef6ab5c
cbeaufil <cbeaufil@localhost.localdomain>
2025-06-19 18:33:07 +0200

    DONE: Test

--- tests/main.c
+++ tests/main.c
@@ -3,41 +3,14 @@
 
 int main() {
 
-    // for (size_t i = 0 ; i < 0x10; i ++) {
-    //     char *p = ft_malloc(0x50);
-    //     if (p) {
-    //         strcpy(p,  "AAAAA");
-    //     }
-    //     printf(">> %p\n", p );
-    // }
+    for (size_t i = 0 ; i < 0x1f; i ++) {
+        char *p = ft_malloc(0x90);
+        if (p) {
+            strcpy(p,  "AAAAA");
+        }
+        printf(">> %p\n", p );
+    }
 
-    // char *p1 = ft_malloc(0x50);
-    //         // strcpy(p,  "AAAAA");
-
-    // char *p2 = ft_malloc(0x50);
-    // char *p3 = ft_malloc(0x50);
-    // char *p4 = ft_malloc(0x50);
-
-    // ft_free(p1);
-    // ft_free(p2);
-    // ft_free(p3);
-    // ft_free(p4);
-
-
-    // p1 = ft_malloc(0x50);
-    // p2 = ft_malloc(0x50);
-    // p3 = ft_malloc(0x50);
-    // p4 = ft_malloc(0x50);
-
-
-    char *p1 = ft_malloc(0x150);
-    char *p2 = ft_malloc(0x150);
-    char *p3 = ft_malloc(0x150);
-
-
-    ft_free(p1);
-    ft_free(p2);
-    ft_free(p3);
 
     return 0;
 }
