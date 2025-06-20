3838979f1067797b89d48072b5cfe62a220796fe
cbeaufil <cbeaufil@42lehavre.fr>
2025-06-20 10:14:16 +0200

    DONE: Tests

--- tests/main.c
+++ tests/main.c
@@ -2,17 +2,36 @@
 #include <string.h>
 #include <stdlib.h>
 
+void    fill(char *buf, size_t n, char c) {
+    while (n) {
+        *buf = c;
+        buf ++;
+        n --;
+    }
+}
+
 int main() {
+    char *fasts[100] = {0};
+    char *small[100] = {0};
 
-    char *p1 = ft_malloc(0x40);
 
-    while (p1) {
+    for (size_t i = 0; i < 10; i ++) {
+        fasts[i] = ft_malloc(0x40);
+        fill(fasts[i], 0x40, 'A');
 
-        strcpy(p1, "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
-        p1 = ft_malloc(0x40);
-        printf("%p\n", p1);
+        small[i] = ft_malloc(0x170);
+        fill(small[i], 0x170, 'B');
+    }
+
+
+
+    for (size_t i = 0; i < 10; i ++) {
+        ft_free(fasts[i]);
+
+        ft_free(small[i]);
 
     }
+
 
 
 
